55b23c07da3ed6c5a8ffd4332db881e9ed26d1ce
Cezary Bober <ceski23@gmail.com>
2021-10-13 15:55:41 +0200

    refactor: cleanup folder structure

--- src/views/App.js
+++ src/views/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
-import './App.scss';
+import logo from 'assets/logo.svg';
+import s from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <div className={s.App}>
+      <header className={s['App-header']}>
+        <img src={logo} className={s['App-logo']} alt="logo" />
+        <p className={s['App-header']}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={s['App-link']}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
